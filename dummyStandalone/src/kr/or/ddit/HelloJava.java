078f2a5092e427518bd2258fe329bfe930cc518c
YoungHyoKim90 <younghyo1101@gmail.com>
2023-05-24 21:45:23 +0900

    second commit

---
+++ dummyStandalone/src/kr/or/ddit/HelloJava.java
@@ -0,0 +1,10 @@
+package kr.or.ddit;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java, 한글 자바");
+	}
+//이클립스가 내부적으로 -d 와 -cp를 사용
+//jre까지 들어가 있슴둥
+}
